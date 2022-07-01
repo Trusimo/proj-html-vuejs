8726894e87932aca11b390895b4d0b7c974938dd
Trusimo <simonetrunfio96@gmail.com>
2022-07-01 14:46:23 +0200

    flex in header

--- src/components/TheHeader.vue
+++ src/components/TheHeader.vue
@@ -1,14 +1,28 @@
 <template>
+    <div class="flex">
+        <img src="../../public/avada-marketing-logo-2x-200x37.png" alt="">
 
+        <!-- ciclo per le voci della navbar -->
+        <div>Ciao</div>
+
+        <div>Numero tel</div>
+        <button>Free Quote</button>
+    </div>
 
 </template>
 
 
 <script>
+export default {
+    name: 'TheHeader',
+    data() {
+        return {
+        
+}},
+    methods: {
 
-
-
-
+    }
+}
 
 
 </script>
@@ -20,7 +34,9 @@
 
 
 <style>
-
+.flex {
+    display: flex;
+}
 
 
 
