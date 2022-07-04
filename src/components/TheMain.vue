1b290c2594669c7b1f545bb74b47648137de4af6
Trusimo <simonetrunfio96@gmail.com>
2022-07-04 13:48:53 +0200

    adds fontawesome

--- src/components/TheMain.vue
+++ src/components/TheMain.vue
@@ -52,6 +52,56 @@
             <button class="btn-orange">(555) 802-134</button>
         </div>
 
+        <div class="padding-sec">
+            <h2 class="text-center">Trusted by Leading Organizations</h2>
+            <div class="flex-partner row">
+                <div class="col" v-for="(item, index) in caseStudies" :key="index">
+                    <img :src="item.img" alt="">
+                    <div class="bold">{{ item.par }}</div>
+                    <h3>{{ item.number }}</h3>
+                    <div class="font-small">{{ item.result }}</div>
+                </div>
+            </div>
+            <button type="submit" class="btn-orange-col text-center">Read More Case Studies</button>
+        </div>
+
+        <div class="padding-sec-lat">
+            <div class="row my-5">
+                <div class="col">
+                    <h2>We Boost Our Clients Bottom Line by Optimizing Their Growth Potential</h2>
+                    <p>Lorem ipsum dolor, sit amet consectetur adipisicing elit. Doloribus dolorem, ducimus illo nostrum odit delectus</p>
+                    <button type="submit" class="btn-orange-col">Why Work With Us</button>
+                </div>
+                <div class="col">
+                    <img src="../../public/we-boost-our-clients-400x366.jpg" alt="">
+                </div>
+            </div>
+        </div>
+
+        <div class="padding-sec-lat">
+            <div class="row">
+                <div class="col">
+                    <div class="padding-col">
+                        <h4>We Build Relationships</h4>
+                        <p class="font-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, libero id totam iusto esse impedit modi</p>
+                    </div>
+                    <div class="padding-col">
+                        <h4>Proprietary Processes</h4>
+                        <p class="font-small">Lorem ipsum dolor sit amet consectetur adipisicing elit. Consequuntur, libero id totam iusto esse impedit modi</p>
+                    </div>
+                </div>
+                
+                <div class="col padding-col">
+                    <div class="flex">
+                        <!-- iconafontawesome -->
+                        <div class="v-for">
+
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+
     </div>
 
 </template>
@@ -69,6 +119,26 @@ export default {
                 {url: require('../../public/hemisferio.png')},
                 {url: require('../../public/spaces.png')},
                 {url: require('../../public/digitalbox.png')},
+            ],
+            caseStudies: [
+                {
+                    img: "../../public/case-studies-1.jpg",
+                    par: "How Spaces attracted five millions visitors by improving the content",
+                    number: "200%",
+                    result: "Higher revenue from digital"
+                },
+                {
+                    img: "../../public/case-studies-4.jpg",
+                    par: "Creativity helped Hemisferio to increase their brand reach vertically",
+                    number: "10x",
+                    result: "Sales increase with the same AD spend"
+                },
+                {
+                    img: "../../public/case-studies-6.jpg",
+                    par: "How DigitalBox used AI-powered data insights to boost sales",
+                    number: "3-year",
+                    result: "Partnership with Avada Consultant"
+                },
             ]
         
 }},
@@ -77,16 +147,7 @@ export default {
     }
 }
 
-
-
-
-
-
-
 </script>
-
-
-
 
 
 
@@ -105,12 +166,29 @@ export default {
     padding: 100px 0;
 }
 
+.padding-sec-lat {
+    padding: 100px 50px;
+}
+
 .btn-orange-col {
     background-color: #f76210;
     color: white;
     border-style: none;
     border-radius: 8px;
     padding: 8px 30px;
+}
+
+.bold {
+    font-weight: bold;
+    font-size: 18px;
+}
+
+.font-small {
+    font-size: 12px;
+}
+
+.padding-col {
+    padding: 10px 30px;
 }
 
 </style>
